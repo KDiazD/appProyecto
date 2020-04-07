64141a89be8c7ed78d54f3c2ac6354480871b1eb
KDiazD <47615707+KDiazD@users.noreply.github.com>
2020-04-07 09:29:35 -0500

    Add project files.

---
+++ appproyecto/appproyecto/Views/Home.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using appproyecto.Views;
+
+namespace appproyecto.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Home : MasterDetailPage
+    {
+        public Home()
+        {
+            InitializeComponent();
+
+            this.Master = new Master();
+            this.Detail = new NavigationPage(new Detail());
+
+            App.MasterD = this;
+        }
+
+
+    }
+}
---
+++ appproyecto/appproyecto/Views/Master.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using appproyecto.Views;
+
+namespace appproyecto.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Master : ContentPage
+    {
+        public Master()
+        {
+            InitializeComponent();
+        }
+
+        private async void historia_neiva_Clicked(object sender, EventArgs e)
+        {
+            App.MasterD.IsPresented = false;
+            await App.MasterD.Detail.Navigation.PushAsync(new Historia());
+
+        }
+
+        private async void lugares_neiva_Clicked(object sender, EventArgs e)
+        {
+            App.MasterD.IsPresented = false;
+            await App.MasterD.Detail.Navigation.PushAsync(new Lugares());
+        }
+
+        private async void comidas_tipicas_neiva_Clicked(object sender, EventArgs e)
+        {
+            App.MasterD.IsPresented = false;
+            await App.MasterD.Detail.Navigation.PushAsync(new Comidas());
+
+        }
+
+        private async void fiestas_tipicas_neiva_Clicked(object sender, EventArgs e)
+        {
+            App.MasterD.IsPresented = false;
+            await App.MasterD.Detail.Navigation.PushAsync(new Fiestas());
+        }
+    }
+}
